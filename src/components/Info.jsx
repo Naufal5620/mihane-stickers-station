50e67610de0e239ebc5608173637e9eb2759a5b4
mihane-ichinose <stevenjinshitian@gmail.com>
2025-03-28 22:46:20 -0400

    added update log in info panel

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,6 +10,15 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import Divider from "@mui/material/Divider";
+
+const SmallListItemText = styled(ListItemText)({
+  "& .MuiTypography-root": {
+    fontSize: "0.85rem",
+    lineHeight: 1.2,
+  },
+});
 
 export default function Info({ open, handleClose }) {
   return (
@@ -23,6 +32,23 @@ export default function Info({ open, handleClose }) {
         <DialogTitle id="alert-dialog-title">Info</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
+
+            {/* Update Log Section */}
+            <Typography variant="h6" component="h3" style={{ marginTop: '10px' }}>
+              Update Log
+            </Typography>
+            <List>
+              <ListItem>
+                <SmallListItemText primary="v0.0.2 - Update Genshin Impact - Paimon's Paintings to Set 14." />
+              </ListItem>
+              <ListItem>
+                <SmallListItemText primary="v0.0.1 - Added Game Filtering Tag." />
+              </ListItem>
+            </List>
+
+            <Divider sx={{ mt: 10, mb: 2 }} />
+
+            {/* GitHub Link */}
             <Typography variant="h6" component="h3">
               A miHoYo, Kuro games sticker maker from here:
             </Typography>
